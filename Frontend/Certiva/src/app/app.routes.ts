e750059d14cd381e38e19bf47c1c23b2777c2a08
Nataly-07 <natalyforero07@gmail.com>
2026-05-11 22:51:24 -0500

    Componente Eventos

--- Frontend/Certiva/src/app/app.routes.ts
+++ Frontend/Certiva/src/app/app.routes.ts
@@ -2,11 +2,16 @@ import { Routes } from '@angular/router';
 import { Registro } from './pages/login/registro/registro';
 import { Login } from './pages/login/login/login';
 import { Home } from './pages/home/home/home';
+import { Eventos } from './pages/eventos/eventos';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: Login },
-  { path: 'register', component: Registro },
-  { path: 'home', component: Home }
+  { path: 'registrase', component: Registro },
+  {
+    path: 'home', component: Home, children: [
+      { path: 'eventos', component: Eventos }
+    ]
+  }
 ];
 
