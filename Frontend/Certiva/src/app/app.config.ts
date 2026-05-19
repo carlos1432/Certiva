72c5a7477fe6b06d30e7a7c90f70e2fae4d9a3e2
Nataly-07 <natalyforero07@gmail.com>
2026-05-18 21:48:37 -0500

    Integración con el Backend para el Login

--- Frontend/Certiva/src/app/app.config.ts
+++ Frontend/Certiva/src/app/app.config.ts
@@ -3,12 +3,17 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+/* import { provideForms } from '@angular/forms'; */
+import { importProvidersFrom } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient()
+    provideHttpClient(),
+    /* provideForms() */
+    importProvidersFrom(ReactiveFormsModule)
   ]
 };
--- Frontend/Certiva/src/app/pages/login/login/login.ts
+++ Frontend/Certiva/src/app/pages/login/login/login.ts
@@ -1,12 +1,43 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { AuthService } from '../../../Services/auth.service';
 
 @Component({
   selector: 'app-login',
-  imports: [RouterLink],
+  standalone: true,
+  imports: [RouterLink, FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
 export class Login {
 
+  email: string = '';
+  password: string = '';
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  login() {
+
+    const data = {
+      correo: this.email,
+      contraseña: this.password
+    };
+
+    this.authService.login(data).subscribe({
+
+      next: (res: any) => {        
+        console.log('LOGIN OK', res);
+        localStorage.setItem('user', JSON.stringify(res));
+
+        this.router.navigate(['/home']);
+      },
+
+      error: (err: any) => {
+        console.error('ERROR LOGIN', err);
+      } 
+    });
+  }
 }
